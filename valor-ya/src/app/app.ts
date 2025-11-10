45c5f5e4c74efbe30e200eacc95719647d91264a
cesarcote <cesarjosecote@gmail.com>
2025-11-10 15:29:47 -0500

    feat: add service area component with help resources and styling

--- valor-ya/src/app/app.ts
+++ valor-ya/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
 import { Footer } from './shared/components/footer/footer.component';
+import { ServiceAreaComponent } from './shared/components/service-area/service-area';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, ServiceAreaComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
